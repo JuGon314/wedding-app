276ec0d8741fbc0957d0011aacfbc2ff6cc89bb6
Ana <progamacaocarol2021@gmail.com>
2024-12-02 03:54:34 -0300

    commit para branch front

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+export function Navbar(){
+    return(
+            <nav>
+                <div>
+                    <h1>Patricia & Rafael</h1>
+                </div>
+            </nav>
+    );
+    
+}
